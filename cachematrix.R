b860efee9fb73267fe8fdc1bfcffc55c941c622c
Rebecca-Dsouza <68778829+Rebecca-Dsouza@users.noreply.github.com>
2020-07-25 19:56:26 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,36 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+  a <- NULL
+  set <- function(y){
+  m <<- n
+  a <<- NULL
+  }
+  get <- function() {m}
+  setInverse <- function(inverse) {a <<- inverse}
+  getInverse <- function() a
+  list(set = set, get = get, 
+  setInverse = setInverse, 
+  getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##Please include your own comment to explain your code (Required in Rubric)
+
+cacheSolve <- function(m, ...) {
+  a <- x$getInverse()
+  if(!is.null(j)){
+  message("getting cached data")
+  return(a)
+  }
+ data <- x$get()
+  a <- solve(mat,...)
+  m$setInverse(a)
+  a
 }
+
+
+
+
